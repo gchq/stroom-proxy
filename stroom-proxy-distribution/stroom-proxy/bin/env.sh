b9311310ffb2879548f67978b98e0f7a429c915b
stroomdev66 <stroomdev66@gmail.com>
2017-05-22 17:10:54 +0100

    Issue #15 : Removed default memory options from env.sh.

--- stroom-proxy-distribution/stroom-proxy/bin/env.sh
+++ stroom-proxy-distribution/stroom-proxy/bin/env.sh
@@ -2,4 +2,4 @@ ulimit -v unlimited
  
 export MALLOC_ARENA_MAX=4
 
-export JAVA_OPTS="${JAVA_OPTS} -Djava.awt.headless=true -Xms512m -Xmx1g -XX:PermSize=256m -XX:MaxPermSize=512m @@JAVA_OPTS@@"
+export JAVA_OPTS="${JAVA_OPTS} -Djava.awt.headless=true @@JAVA_OPTS@@"
