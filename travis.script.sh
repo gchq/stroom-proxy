e58eb74a93750c26a78c46368aeb12b56341998f
at055612 <22818309+at055612@users.noreply.github.com>
2021-04-27 11:31:50 +0100

    Update travis script for sonatype

--- travis.script.sh
+++ travis.script.sh
@@ -3,26 +3,36 @@
 #exit script on any error
 set -e
 
-#Shell Colour constants for use in 'echo -e'
-#e.g.  echo -e "My message ${GREEN}with just this text in green${NC}"
-RED='\033[1;31m'
-GREEN='\033[1;32m'
-YELLOW='\033[1;33m'
-BLUE='\033[1;34m'
-NC='\033[0m' # No Colour 
+# shellcheck disable=SC2034
+{
+  #Shell Colour constants for use in 'echo -e'
+  #e.g.  echo -e "My message ${GREEN}with just this text in green${NC}"
+  RED='\033[1;31m'
+  GREEN='\033[1;32m'
+  YELLOW='\033[1;33m'
+  BLUE='\033[1;34m'
+  NC='\033[0m' # No Colour 
+}
 
+EXTRA_BUILD_ARGS=()
 #establish what version we are building
 if [ -n "$TRAVIS_TAG" ]; then
     #Tagged commit so use that as our version, e.g. v1.2.3
     PRODUCT_VERSION="${TRAVIS_TAG}"
 
-    #upload the maven artefacts to bintray
-    EXTRA_BUILD_ARGS="bintrayUpload"
+    # GPG sign the artifacts, publish to nexus then close and release
+    # the staging repo to the public nexus repo and on to central
+    EXTRA_BUILD_ARGS=(
+      "signMavenJavaPublication"
+      "publishToSonatype"
+      "closeSonatypeStagingRepository"
+      #"closeAndReleaseSonatypeStagingRepository"
+    )
 else
     #No tag so use the branch name as the version, e.g. master-SNAPSHOT
     #None tagged builds are NOT pushed to bintray
     PRODUCT_VERSION="${TRAVIS_BRANCH}-SNAPSHOT"
-    EXTRA_BUILD_ARGS=""
+    EXTRA_BUILD_ARGS=()
 fi
 
 #Dump all the travis env vars to the console for debugging
@@ -35,4 +45,4 @@ echo -e "TRAVIS_EVENT_TYPE:    [${GREEN}${TRAVIS_EVENT_TYPE}${NC}]"
 echo -e "PRODUCT_VERSION:      [${GREEN}${PRODUCT_VERSION}${NC}]"
 
 #Run the build (including running maven install task to generate poms
-./gradlew -Pversion=$PRODUCT_VERSION clean build ${EXTRA_BUILD_ARGS}
+./gradlew -Pversion="${PRODUCT_VERSION}" clean build "${EXTRA_BUILD_ARGS[@]}"
